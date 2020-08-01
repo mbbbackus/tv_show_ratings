20168ccb6c41ba28c2fd52bd4095f87ce671f6ba
Michael Benjamin Backus <mbbbackus@gmail.com>
2020-08-01 19:48:53 -0400

    add database populator

--- tv_ratings_website/ratings_api/models.py
+++ tv_ratings_website/ratings_api/models.py
@@ -1,25 +1,26 @@
+import uuid
 from django.db import models
 
 class Series(models.Model):
-	id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+	id = models.CharField(max_length=9, primary_key=True, editable=False)
 	primary_title = models.CharField(max_length=200)
 	original_title = models.CharField(max_length=200)
 	start_year = models.IntegerField(default=1775)
 	end_year = models.IntegerField(default=2175) #debugging purposes
 
 class Episode(models.Model):
-	id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-	series = models.ForeignKey(Series)
+	id = models.CharField(max_length=9, primary_key=True, editable=False)
+	series = models.ForeignKey(Series, on_delete=models.CASCADE)
 	season_number = models.IntegerField(default=-1)
 	episode_number = models.IntegerField(default=-1)
 
 class EpisodeRating(models.Model):
-	episode = models.ForeignKey(Episode)
+	episode = models.ForeignKey(Episode, on_delete=models.CASCADE)
 	average_rating = models.FloatField(default=-1.0)
 	num_votes = models.IntegerField(default=-1)
 
 
 class EpisodeName(models.Model):
-	episode = models.ForeignKey(Episode)
+	episode = models.ForeignKey(Episode, on_delete=models.CASCADE)
 	primary_name = models.CharField(max_length=200)
 	original_name = models.CharField(max_length=200)
--- tv_ratings_website/tv_ratings_website/settings.py
+++ tv_ratings_website/tv_ratings_website/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'ratings_api.apps.RatingsApiConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
