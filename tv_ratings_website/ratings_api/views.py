7f895afe8f29f13e261da6f4dfd27975d9717269
Michael Benjamin Backus <mbbbackus@gmail.com>
2020-08-04 20:32:10 -0400

    Fix whitespace error

--- tv_ratings_website/ratings_api/views.py
+++ tv_ratings_website/ratings_api/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
-from rest_framework import filters, generics, viewsets
+from rest_framework import filters, generics, viewsets, mixins
 from .models import Series, Episode, EpisodeName, EpisodeRating
 from .serializers import SeriesSerializer, EpisodeSerializer, EpisodeNameSerializer, EpisodeRatingSerializer
 
-class SeriesView(viewsets.ModelViewSet):
-    search_fields = ['primary_title']
+# class SeriesView(viewsets.ModelViewSet):
+# Some shows get better, more specific search results than others
+# if you search by primary title vs original title 
+# so the frontend will make API calls for both searches
+class SeriesView(mixins.ListModelMixin, viewsets.GenericViewSet):
     filter_backends = (filters.SearchFilter,)
     queryset = Series.objects.all()
     serializer_class = SeriesSerializer
+
+# example call: /api/series_by_original_title/?search=House%20M.D.
+class SeriesByOriginalTitleView(SeriesView):
+    search_fields = ['original_title']
+
+# example call: /api/series_by_primary_title/?search=House
+class SeriesByPrimaryTitleView(SeriesView):
+    search_fields = ['primary_title']
+
--- tv_ratings_website/tv_ratings_website/urls.py
+++ tv_ratings_website/tv_ratings_website/urls.py
@@ -4,7 +4,8 @@ from rest_framework import routers
 from ratings_api import views
 
 router = routers.DefaultRouter()
-router.register(r'ratings', views.SeriesView, 'series')
+router.register(r'series_by_original_title', views.SeriesByOriginalTitleView, 'series_original_title')
+router.register(r'series_by_primary_title', views.SeriesByPrimaryTitleView, 'series_primary_title')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
