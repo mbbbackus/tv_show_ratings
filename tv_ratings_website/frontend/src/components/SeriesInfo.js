64e8dc422a51365b0f74a7062a0b0d864d94e53c
Michael Benjamin Backus <mbbbackus@gmail.com>
2020-08-13 13:26:17 -0400

    label x axes and fix tooltip info

--- tv_ratings_website/frontend/src/components/SeriesInfo.js
+++ tv_ratings_website/frontend/src/components/SeriesInfo.js
@@ -1,12 +1,15 @@
 import React, { Component } from "react";
-import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip } from 'recharts';
+import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip, Label } from 'recharts';
 
-const CustomTooltip = ({ active, payload, label, info }) => {
+const CustomTooltip = ({ active, payload, label, info, filteredEps }) => {
   let episode = {};
   for (let i = 0; i < info.episodes.length; i++) {
     // TODO: find episode by id, not name
     if (info.episodes[i].name === label) {
       episode = info.episodes[i]; 
+      break;
+    } else if (filteredEps[i].name === label) {
+      episode = info.episodes[i];
       break;
     }
   }
@@ -14,8 +17,8 @@ const CustomTooltip = ({ active, payload, label, info }) => {
     return (
       <div className="custom-tooltip">
         <p>{`Season ${episode.season_number} Episode ${episode.episode_number}`}</p>
-        <p>{`"${label}"`}</p>
-        <p>{`rating : ${payload[0].value}`}</p>
+        <p>{`"${episode.name}"`}</p>
+        <p>{`rating : ${episode.average_rating}`}</p>
         <p>{`votes : ${episode.num_votes}`}</p>
       </div>
     );
@@ -29,12 +32,13 @@ class SeriesInfo extends Component {
     super(props);
     this.renderPopularEpisodes = this.renderPopularEpisodes.bind(this);
     this.renderUnpopularEpisodes = this.renderUnpopularEpisodes.bind(this);
+    this.filterEpisodes = this.filterEpisodes.bind(this);
     this.xTicks = this.xTicks.bind(this);
   }
-  xTicks() {
+  xTicks(eps) {
     let ticks = [];
-    for (let i = 0; i < this.props.info.episodes.length; i++) {
-      let ep = this.props.info.episodes[i];
+    for (let i = 0; i < eps.length; i++) {
+      let ep = eps[i];
       if (ep.episode_number === 1){
         ticks.push(ep.name);
       } 
@@ -65,23 +69,42 @@ class SeriesInfo extends Component {
       </div>
     ));
   }
+  filterEpisodes(episodes) {
+    let newEps = [];
+    for (let i = 0; i < episodes.length; i++) {
+      let ep = {...episodes[i]};
+      if (ep.episode_number === 1){
+        ep.name = JSON.stringify(ep.season_number);
+      } 
+      newEps.push(ep);
+    }
+    return newEps;
+  }
   render () {
+    let filteredEpisodes = this.filterEpisodes(this.props.info.episodes);
     return (
       <div className="series-info">
         <a href="/search">Back To Search</a>
-        <h1>{this.props.info.primary_title} ({this.props.info.start_year} - {this.props.info.end_year !== -1 ? this.props.info.end_year : '?'})</h1>
-        <div>
-          <LineChart width={800} height={400} data={this.props.info.episodes}>
+        <h1>
+          {this.props.info.primary_title + ' '} 
+          ({this.props.info.start_year + ' '}
+          - 
+          {' ' + (this.props.info.end_year !== -1 ? this.props.info.end_year : 'present')})
+        </h1>
+        <div >
+          <LineChart margin={{bottom: 25}} width={800} height={420} data={filteredEpisodes}>
             <Line type="monotone" dataKey="average_rating" stroke="black" dot={false}/>
             <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
             <XAxis 
               dataKey="name" 
               interval={0}
-              ticks={this.xTicks()}
-              padding={{left:30, right:30}}
-            />
+              ticks={this.xTicks(filteredEpisodes)}
+              padding={{left:20, right:20}}
+            >
+              <Label value="Season" position="bottom" offset={5} />
+            </XAxis>
             <YAxis domain={[0,10]} ticks={[0,2,4,6,8,10]}/>
-            <Tooltip content={<CustomTooltip info={this.props.info}/>}/>
+            <Tooltip content={<CustomTooltip info={this.props.info} filteredEps={filteredEpisodes}/>}/>
           </LineChart>
         </div>
         <div className="random-container"> 
